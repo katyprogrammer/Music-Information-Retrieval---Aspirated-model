5993432dec70bf7c9e25c28180a976b79b164e24
Katy <katyprogrammer@gmail.com>
2016-06-15 18:41:45 +0800

    Update CombinedAspirated

--- combineAspirated.m
+++ combineAspirated.m
@@ -6,47 +6,47 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% follow geneAsp2.m
-%% make a spectrogram, and do inverse fourier transform to get the audio file
-function sig = geneAsp2(pitch,dur,volumn)
-    Fs = 65536;
-    N = round(dur * Fs);
-    time = (0:N-1) / Fs;
-    sig = cos(2*pi*pitch*time);
-%     w = 1024;
-%     h = w/8;
-%     nfft = w;
-%     spec = stft(sig, w, h, nfft, Fs);
-    
-    spec = fft(sig,Fs);
-    spec = spec(1:end/2);
-    spec_abs = abs(spec);
-    spec_angle = angle(spec);
-    x = linspace(0,Fs/2,length(spec));
-    [pitchval, pitchidx] = max(spec_abs);
-    
-    spec_abs = zeros(1,length(spec_abs));
-    a = 10;
-    b = 100;
-    d = 10;
-    e = 2;
-    sampnum = length(spec)/d;
-    for i = 1:sampnum
-        p = round(rand * (length(spec)-1))+1;
-        if pitchidx ~= p
-            spec_abs(p) = pitchval * b / ( ( abs ( x(p) - pitch ) ^ e ) * a + b );
-%             spec_abs(p) = pitchval / 1000;
-        end
-    end
-    norm(spec_abs)
-    spec_abs = volumn * spec_abs / norm(spec_abs);
-    spec_ = complex(spec_abs.*cos(spec_angle), spec_abs.*sin(spec_angle));
-    sig_ = ifft(spec_abs,Fs);
-    sig = sig_;
-    
-    l = round(length(sig)*((1-dur)/2)); r = round(length(sig)*((1-dur)/2+dur));
-    sig = sig(l:r);
-    sig = sig .* hanning(length(sig))';
-    
-    plot(x,spec_abs);
-    axis([0, 2000,0, pitchval]);
-    endz
+
+
+%[sig, fs] = audioread('audioclip.wav');
+% p=audioplayer(sig, fs); playblocking(p);
+fragmentsNum = 100;
+fs = 65536;
+% [wholeSig, fs] = audioread('audioclip.wav');
+% p=audioplayer(sig, fs); playblocking(p);
+%% import audio file of songs
+PATH_SONG = './song'
+listOfSongs = listfile(PATH_SONG)
+song = [];
+for i = 1:length(listOfSongs)
+    % for audioIndex = 1:fragmentsNum
+        s% ignal = wholeSig(audioIndex : audioIndex * round(( length(wholeSig) / fragmentsNum )));
+        signal = wholeSig;
+        w = 1024;
+        h = w/8;
+        nfft = w; % size of the fft
+        spec = fft(signal, fs);
+        % spec = stft(sig, w, h, nfft, fs);
+        % generate aspirated model and add it on audio 
+
+        size(geneAspSpectrogram(440, (size(signal) / 65536), 100))
+        size(spec_abs)
+        spec_abs = abs(spec);
+        spec_abs = spec_abs + (geneAspSpectrogram(440, (size(signal) / 65536), 100))';
+        spec_angle = angle(spec);
+        x = linspace(0,fs/2,w/2+1);
+        % reconstruct the audio through iSTFT
+        spec_ = complex(spec_abs.*cos(spec_angle), spec_abs.*sin(spec_angle));
+        sig_ = istft(spec_, h, nfft, fs);
+        filename = 'combinedAspirated.wav';
+        audiowrite(filename,sig_,fs);
+        p = audioplayer(sig_, fs);
+        playblocking(p);
+        song = [song; sig_];
+    % end
+     
+end
+player = audioplayer(song, fs);
+playblocking(p);
+
+
---
+++ geneAspSpectrogram.m
@@ -0,0 +1,46 @@
+%% make a spectrogram, and do inverse fourier transform to get the audio file
+function spec_abs = geneAspSpectrogram(pitch,dur,volumn)
+    Fs = 65536;
+    N = round(dur * Fs);
+    time = (0:N-1) / Fs;
+
+    sig = cos(2*pi*pitch*time);
+%     w = 1024;
+%     h = w/8;
+%     nfft = w;
+%     spec = stft(sig, w, h, nfft, Fs);
+    
+    spec = fft(sig,Fs);
+    spec = spec(1:end/2);
+    spec_abs = abs(spec);
+    spec_angle = angle(spec);
+    x=linspace(0,Fs/2,length(spec));
+    [pitchval, pitchidx] = max(spec_abs);
+    
+    spec_abs = zeros(1,length(spec_abs));
+    %%spec_abs(pitchidx) = pitchval;
+    a = 10;
+    b = 100;
+    d = 10;
+    m = 5;
+    e = 1;
+    sampnum = length(spec)/d;
+    for i = 1:sampnum
+        p = round(rand * (length(spec)-1))+1;
+        if pitchidx ~= p
+            spec_abs(p) = pitchval * b / ( ( abs ( x(p) - pitch ) ^ e ) * a + b )/m;
+        end
+    end
+    spec_abs = volumn * spec_abs / norm(spec_abs);
+    
+    spec_ = complex(spec_abs.*cos(spec_angle), spec_abs.*sin(spec_angle));
+    sig_ = ifft(spec_abs,Fs);
+    sig = sig_;
+    
+    l = round(length(sig)*((1-dur)/2)); r = round(length(sig)*((1-dur)/2+dur));
+    sig = sig(l:r);
+    sig = sig .* hanning(length(sig))';
+    
+    plot(x,spec_abs);
+    axis([0, 2000,0, pitchval]);
+end
