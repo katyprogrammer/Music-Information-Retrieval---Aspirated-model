04046d8aeec25c3fe188f06275d8810922380dd6
Katy <katyprogrammer@gmail.com>
2016-06-15 12:32:39 +0800

    generate aspirated model

--- extractAspirated.m
+++ extractAspirated.m
@@ -25,11 +25,9 @@ epsilon = 0.01
 for i = 1:length(spec)
     meanValue = mean(spec_abs(:,i));
     deviation = std(spec_abs(:,i));
-%     spec_abs(spec_abs(:,i)>th,i) = th;
     spec_abs(:,i) = spec_abs(:,i) - meanValue;
-    spec_abs(spec_abs(:,i)>deviation,i) = 0;
-    
-%     findpeaks(spec_abs(:,i));
+    spec_abs(spec_abs(:,i)>deviation,i) = 0;    
+    findpeaks(spec_abs(:,i));
      axis([0,100,0,20]);
      plot(x,spec_abs(:,i));
      axis([0,3000,0,20]);
@@ -39,10 +37,13 @@ spec_ = complex(spec_abs.*cos(spec_angle), spec_abs.*sin(spec_angle));
 sig_ = istft(spec_, h, nfft, fs);
 filename = 'aspirated.wav';
 audiowrite(filename,sig_,fs);
+siz = wavread(filename,'size') %siz = [samples channels]
+disp('audio length')
+siz(1)/Fs %should give you the length in seconds
 aspirated = audioread(filename);
 size(aspirated)
-p=audioplayer(sig_, fs);
+p = audioplayer(sig_, fs);
 playblocking(p);
-geneAsp(440, 1, 100);
-SDR = bss_eval_sources(audioread(filename), audioread('geneAsp.wav'))
+% geneAsp(440, 2.7637, 100);
+% SDR = bss_eval_sources(audioread(filename), audioread('geneAsp.wav'))
 
